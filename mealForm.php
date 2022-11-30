96516dc0f679c11cdb142071613dd189f7090469
gbs2gps <89879362+gbs2gps@users.noreply.github.com>
2022-11-30 01:31:11 -0500

    Update mealForm.php

--- mealForm.php
+++ mealForm.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Nutrilytics</title>
+    <title>Nutrily=itics</title>
     <link rel="stylesheet" href="admin.css">
 </head>
 
@@ -11,7 +11,7 @@
 
   	<div class="sec-center"> 	
 	  	<input class="dropdown" type="checkbox" id="dropdown" name="dropdown"/>
-	  	<label class="for-dropdown" for="dropdown">Nutrilytics Menu <i class="uil uil-arrow-down"></i></label>
+	  	<label class="for-dropdown" for="dropdown">Nutrilitics Menu <i class="uil uil-arrow-down"></i></label>
 
       <div class="section-dropdown"> 
   			<a href="index.php">Home</a>
