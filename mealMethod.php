94b6243d44f39d284e9dcc54fbba817015189733
matt-koehler <78246957+matt-koehler@users.noreply.github.com>
2022-11-30 00:46:56 -0500

    Update mealMethod.php

--- mealMethod.php
+++ mealMethod.php
@@ -2,7 +2,7 @@
 function addMeal($MealName, $Calories, $Serving_Size)
 {
     global $db;
-    $query = "INSERT INTO Meal(MealName, Calories, Serving_Size) VALUES (:MealName,:Calories,:Serving_Size)";  
+    $query = "INSERT INTO Meal(MealName, Calories, Serving_Size) VALUES (:MealName,:Calories,:Serving_Size)"; 
     try {
         $statement = $db->prepare($query);
         $statement->bindValue(':MealName', $MealName);
@@ -10,7 +10,6 @@ function addMeal($MealName, $Calories, $Serving_Size)
         $statement->bindValue(':Serving_Size', $Serving_Size);
         $statement->execute();
         $statement->closeCursor();
-
         // if ($statement->rowCount() == 0)
         //     echo "Failed to add a friend <br/>";
     }
@@ -26,6 +25,23 @@ function addMeal($MealName, $Calories, $Serving_Size)
     catch (Exception $e)
     {
         echo $e->getMessage();
+    }
+    $q2 = "SELECT MAX(MealID) FROM Meal"; 
+    try{
+        $statement = $db->prepare($q2);
+        $statement->execute();
+        $MealID = $statement->fetch();
+        $statement->closeCursor();
+        return $MealID;
+    }
+    catch (PDOException $e) 
+    {
+        // echo $e->getMessage();
+        // if there is a specific SQL-related error message
+        //    echo "generic message (don't reveal SQL-specific message)";
+
+        if (str_contains($e->getMessage(), "Duplicate"))
+		   echo "Failed to add a meal <br/>";
     }
 }
 
@@ -48,7 +64,7 @@ function getMealByID($MealID)
     $statement->bindValue(':MealID', $MealID);
     $statement->execute();
     $result = $statement->fetch(); 
-    $statement->closeCursor();    
+    $statement->closeCursor();  
     return $result;
 }
 function updateMeal($MealID, $MealName, $Calories, $Serving_Size)
@@ -79,4 +95,132 @@ function deleteMeal($MealID)
     $statement->execute();
     $statement->closeCursor();
 }
+
+function addAdds($UserID,$MealID)
+{
+    global $db;
+    $query = "INSERT INTO Adds(UserID, MealID) VALUES (:UserID,:MealID)";  
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':UserID', $UserID);
+        $statement->bindValue(':MealID', $MealID);
+        $statement->execute();
+        $statement->closeCursor();
+
+        // if ($statement->rowCount() == 0)
+        //     echo "Failed to add a friend <br/>";
+    }
+    catch (PDOException $e) 
+    {
+        // echo $e->getMessage();
+        // if there is a specific SQL-related error message
+        //    echo "generic message (don't reveal SQL-specific message)";
+
+        if (str_contains($e->getMessage(), "Duplicate"))
+		   echo "Failed to add a meal <br/>";
+    }
+    catch (Exception $e)
+    {
+        echo $e->getMessage();
+    }
+}
+
+function addEaten($Eat_Name,$Calories)
+{
+    global $db;
+    $query = "INSERT INTO Meals_Eaten(Eat_Name, Calories) VALUES (:Eat_Name,:Calories)";
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':Eat_Name', $Eat_Name);
+        $statement->bindValue(':Calories', $Calories);
+        $statement->execute();
+        $statement->closeCursor();
+        // if ($statement->rowCount() == 0)
+        //     echo "Failed to add a friend <br/>";
+    }
+    catch (PDOException $e) 
+    {
+        // echo $e->getMessage();
+        // if there is a specific SQL-related error message
+        //    echo "generic message (don't reveal SQL-specific message)";
+
+        if (str_contains($e->getMessage(), "Duplicate"))
+		   echo "Failed to add a meal <br/>";
+    }
+    catch (Exception $e)
+    {
+        echo $e->getMessage();
+    }
+    $q2 = "SELECT MAX(EatID) FROM Meals_Eaten"; 
+    try{
+        $statement = $db->prepare($q2);
+        $statement->execute();
+        $EatID = $statement->fetch();
+        $statement->closeCursor();
+        return $EatID;
+    }
+    catch (PDOException $e) 
+    {
+        // echo $e->getMessage();
+        // if there is a specific SQL-related error message
+        //    echo "generic message (don't reveal SQL-specific message)";
+
+        if (str_contains($e->getMessage(), "Duplicate"))
+		   echo "Failed to add a meal <br/>";
+    }
+}
+
+function getMealNameByID($MealID)  
+{
+    global $db;
+    $query = "SELECT MealName FROM Meal where MealID = :MealID";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':MealID', $MealID);
+    $statement->execute();
+    $result = $statement->fetch(); 
+    $statement->closeCursor();  
+    return $result[0];
+}
+function getMealCaloriesByID($MealID)  
+{
+    global $db;
+    $query = "SELECT Calories FROM Meal where MealID = :MealID";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':MealID', $MealID);
+    $statement->execute();
+    $result = $statement->fetch(); 
+    $statement->closeCursor();  
+    return $result[0];
+}
+function addEats($UserID,$EatID)
+{
+    global $db;
+    $query = "INSERT INTO Eats(UserID, EatID) VALUES (:UserID,:EatID)";  
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':UserID', $UserID);
+        $statement->bindValue(':EatID', $EatID);
+        $statement->execute();
+        $statement->closeCursor();
+
+        // if ($statement->rowCount() == 0)
+        //     echo "Failed to add a friend <br/>";
+    }
+    catch (PDOException $e) 
+    {
+        // echo $e->getMessage();
+        // if there is a specific SQL-related error message
+        //    echo "generic message (don't reveal SQL-specific message)";
+
+        if (str_contains($e->getMessage(), "Duplicate"))
+		   echo "Failed to add a meal <br/>";
+    }
+    catch (Exception $e)
+    {
+        echo $e->getMessage();
+    }
+}
+
 ?>
