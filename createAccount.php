8838e201f2d6dbdb21f6531915cbceb0d3658717
gbs2gps <89879362+gbs2gps@users.noreply.github.com>
2022-11-29 23:56:33 -0500

    Add files via upload

---
+++ createAccount.php
@@ -0,0 +1,34 @@
+
+<?php include('../'); ?>
+<html>
+
+<head>
+    <title>Nutrilytics</title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+
+        <div class="logo-dark">Welcome to Nutralytics! Please Create an Account</div>
+         
+        <input class="dark-light" type="checkbox" id="dark-light" name="dark-light"/>
+            <label for="dark-light"></label>
+	
+            <form action="index.php" method="POST" class= "logo-light2">
+                <div class="insert-info">
+                    Password: 
+                    <input type="text" name="Password" placeholder="Enter Password">
+                    <br>
+                    
+                    Daily Calories Goal: 
+                    <input type="text" name="Calories_Goal" placeholder="Enter Daily Calories Goal">
+                    <br>
+</div>
+<br>
+                    <input type="submit" name="submit" value="Login" class="button-submit">
+
+                </form>
+
+                    </div>
+                </input>
+            </div>
+
+       
---
+++ test.php
@@ -0,0 +1,28 @@
+<head>
+    <title>Nutrilytics</title>
+    <link rel="stylesheet" href="test.css">
+</head>
+
+<input class="dark-light" type="checkbox" id="dark-light" name="dark-light"/>
+  	<label for="dark-light"></label>
+
+  	<div class="light-back"></div> 
+
+
+  	<div class="sec-center"> 	
+	  	<input class="dropdown" type="checkbox" id="dropdown" name="dropdown"/>
+	  	<label class="for-dropdown" for="dropdown">Nutrilytics Menu <i class="uil uil-arrow-down"></i></label>
+  		<div class="section-dropdown"> 
+  			<a href="index.php">Home</a>
+		  	<input class="dropdown-sub" type="checkbox" id="dropdown-sub" name="dropdown-sub"/>
+		  	<label class="for-dropdown-sub" for="dropdown-sub">Meals</label>
+	  		<div class="section-dropdown-sub"> 
+	  			<a href="mealForm.php">All Meals</a>
+	  			<a href="your_meals.php">Your Meals</a>
+	  		</div>
+  			<a href="recipeForm.php">Recipes</a>
+  			<a href="ingredients.php">Ingredients</a>
+			<a href="#">Logout</a>
+
+  		</div>
+  	</div>
